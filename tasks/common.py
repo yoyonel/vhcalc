47d2fa7d2d6f99f8acdf43a015a566daf21d1501
ATTY Lionel <yoyonel@hotmail.com>
2024-09-02 15:52:44 +0200

    ci(github-actions): use Setup FFmpeg action and add 'windows-latest' in platform matrix target
    Signed-off-by: ATTY Lionel <yoyonel@hotmail.com>

--- tasks/common.py
+++ tasks/common.py
@@ -1,3 +1,9 @@
+import sys
+
 VENV_PREFIX = "poetry run"
 _COMMON_TARGETS = ["vhcalc", "tests"]
 COMMON_TARGETS_AS_STR = " ".join(_COMMON_TARGETS)
+
+# - [Add pty support for Windows? #561](https://github.com/pyinvoke/invoke/issues/561)
+# - [invoke fails on windows #62](https://github.com/City-Bureau/city-scrapers/issues/62)
+USE_PTY = sys.platform != "win32"
